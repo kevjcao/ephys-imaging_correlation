08066c8c600ac2e1eb67ef70914721eda7669b69
Kevin Cao <kev.j.cao@gmail.com>
2022-06-24 17:40:16 -0400

    Update patch_imaging_correlation.py
    added peak identifiers

--- patch_imaging_correlation.py
+++ patch_imaging_correlation.py
@@ -21,6 +21,23 @@ ref_ch = 0  # 0 = no ref ch from imaging (i.e. AF594 red), 1 = ref ch
 user_input = input("ScanImage framerate (Hz) and trigger delay (s):").split(
     ',')  # user_input[0] = framerate, [1] = delay
 
+
+## define functions
+# finds and annotates max peak
+# borrowed from stackoverflow.com/questions/43374920/how-to-automatically-annotate-maximum-value-in-pyplot
+def annot_max(x,y, ax=None):
+    xmax = x[np.argmax(y)]
+    ymax = y.max()
+    text= "x={:.3f}, y={:.3f}".format(xmax, ymax)
+    if not ax:
+        ax=plt.gca()
+    bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
+    arrowprops=dict(arrowstyle="->",connectionstyle="angle,angleA=0,angleB=60")
+    kw = dict(xycoords='data',textcoords="axes fraction",
+              arrowprops=arrowprops, bbox=bbox_props, ha="right", va="top")
+    ax.annotate(text, xy=(xmax, ymax), xytext=(0.94,0.96), **kw)
+
+
 ## import MatLab .mat file
 file_path = filedialog.askopenfilename()
 mat_contents = sp.loadmat(file_path)
@@ -49,9 +66,18 @@ timestep = 1 / (0.25 / 1000)  # sampling frequency (Hz), from digitizer; double 
 
 freq, Psden = signal.welch(ephys[:, 1], timestep, nperseg=4096)  #welch's periodogram w/ overlapping windowing
 plt.plot(freq, Psden)
-plt.xlim(0,70)
+plt.xlim(0,200)
 plt.xlabel('frequency [Hz]')
 plt.ylabel('PSD [V**2/Hz]')
+plt.show()
+
+## identify max spike frequency in filtered data
+x = freq[1:60]
+y = Psden[1:60]        # only frequencies under 60Hz for max calculation to avoid 60 cycle noise
+fig, ax = plt.subplots()
+ax.plot(x,y)
+annot_max(x,y)
+#ax.set_ylim(0,0.5)
 plt.show()
 
 ## detect and filter 60Hz noise
@@ -65,11 +91,38 @@ plt.plot( freq, 20*np.log10(abs(h)))
 plt.show()
 
 # apply notch filter to signal
-y_notched = signal.filtfilt(b_notch, a_notch, y_pure)
+y_notched = signal.filtfilt(b_notch, a_notch, ephys[:,1])   # apply 60Hz bandpass on spike data
+filtfreq, filtPsden = signal.welch(y_notched, timestep, nperseg=4096)   # run PSD on filtered data
 
 # plot notch-filtered version of signal
-plt.subplot(212)
-plt.plot(t, y_notched, color = 'r')
+ax1 = plt.subplot(211)
+ax1.plot(ephys[:,0] / 1000, y_notched)
+ax1.set_xlabel('time (s)')
+ax1.set_xlim(0, 30)
+ax1.set_ylabel('mV')
+ax2 = plt.subplot(223)
+ax2.plot(filtfreq, filtPsden)
+ax2.set_xlabel('freq (Hz)')
+ax2.set_xlim(0, 80)
+ax2.set_ylabel('PSD [V**2/Hz]')
+ax3 = plt.subplot(224)
+ax3.plot(filtfreq, filtPsden)
+ax3.set_xlabel('freq (Hz)')
+ax3.set_xlim(0, 20)
+ax3.set_ylabel('PSD [V**2/Hz]')
+plt.show()
+
+## identify max spike frequency in filtered data
+# this is a redundant measure but may mark other oscillatory signals
+x = filtfreq[1:100]
+y = filtPsden[1:100]        # only frequencies under 100Hz for max calculation
+
+fig, ax = plt.subplots()
+ax.plot(x,y)
+annot_max(x,y)
+#ax.set_ylim(0,0.5)
+plt.show()
+
 
 ## check correlation between ephys and imaging
 # **caution - requires resampling of ephys due to lower sampling rate of imaging data
@@ -105,16 +158,3 @@ ax_gcp.margins(0, 0.1)
 ax_corr.margins(0, 0.1)
 fig_crosscorr.tight_layout()
 plt.show()
-
-# indices, props = find_peaks(peaks, threshold=1)
-# maxima_heights = props[‘peak_heights’]
-#
-#
-# def peak_calculator(self):
-#
-#     global peaks
-#     self.indices = find_peaks(peaks, threshold=1)[0]
-#
-#     peaks_min = peaks * -1
-#     indices_min = find_peaks(peaks_min, threshold=1)[0]
-#
